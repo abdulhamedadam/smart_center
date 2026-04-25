6f0a42a8a654bb6b265b396d0cb45c986fd5f766
Abdulhamed Zaghloul <82878328+abdulhamedadam@users.noreply.github.com>
2026-04-25 14:52:01 +0300

    Update Authenticate.php

--- vendor/filament/filament/src/Http/Middleware/Authenticate.php
+++ vendor/filament/filament/src/Http/Middleware/Authenticate.php
@@ -31,7 +31,7 @@ class Authenticate extends Middleware
 
         abort_if(
             $user instanceof FilamentUser ?
-                ( $user->canAccessPanel($panel)) :
+                (! $user->canAccessPanel($panel)) :
                 (config('app.env') !== 'local'),
             403,
         );
